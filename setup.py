f87ff06aa55075d31b64591b0dc270eea9d4c185
François Bissey <francois.bissey@canterbury.ac.nz>
2017-05-16 14:33:10 +1200

    Basic python installer added

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup( name="brial"
+     , version="0.9"
+     , description="Python implementation of BRiAl"
+     , long_description="BRiAl is the legacy implementation of PolyBoRi"
+     , url="https://github.com/BRiAl/BRiAl-python"
+     , license="GPL 2+"
+     , packages=["brial"]
+     )
